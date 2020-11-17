8c07f5aa38e1ccb154e44b1ef445d8d2a5bc4216
matthewapeters <matthewapeters@comcast.net>
2020-11-17 15:22:39 -0800

    adding some tests

---
+++ pane_test.go
@@ -0,0 +1,18 @@
+package dali
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPane(t *testing.T) {
+
+	P := NewPane("TestPaneOne")
+	S := Span{Text: "Test Text", ID: "TestSpanOne"}
+	P.AddElement(S)
+	html := fmt.Sprintf("%s", P)
+	expected := `<div id="TestPaneOne"><span name="TestSpanOne">Test Text</span> </div>`
+	if html != expected {
+		t.Errorf(`expected "%s" but got "%s"`, expected, html)
+	}
+}
